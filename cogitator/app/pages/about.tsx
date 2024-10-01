8e843d370074fae8a747ee2cc8d224dab27bc4d8
crosby7 <53411767+crosby7@users.noreply.github.com>
2024-10-01 00:35:30 -0400

    About page basic - characterCreation start
    - About
    -- Basic styles
    -- Button to link new players to charCreation
    
    - CharacterGenerator
    -- Created a function to generate characteristic rolls (i.e. str, will)
    -- Number is generated successfully and returned
    --- Can be console.logged but not displayed in Text element, likely need to re-render but I'll investigate when I revisit

--- cogitator/app/pages/about.tsx
+++ cogitator/app/pages/about.tsx
@@ -1,31 +1,95 @@
-import { View, Text, Image, Pressable } from 'react-native'
+import { View, Text, Image, Pressable, StyleSheet } from 'react-native'
 import { Link } from 'expo-router'
 import React from 'react'
 
 export default function about() {
   return (
-    <View>
-      <View>
-        <Text>Welcome to Cogitator:</Text>
-        <Text>A Warhammer 40k App Companion</Text>
-        <Text>For the Imperium Maledictum TTRPG</Text>
-        <Text>
+    <View style={styles.container}>
+      <View style={styles.descriptionView}>
+        <Text style={styles.header}>Welcome to Cogitator:</Text>
+        <Text style={styles.subHeader}>A Warhammer 40k App Companion</Text>
+        <Text style={styles.tertHeader}>For the Imperium Maledictum TTRPG</Text>
+        <Text style={styles.descText}>
           This app is under heavy construction. 
           Features are being added as quickly as possible. 
           Existing features may break at any point, but we will work quickly to fix any bugs as they arise.
           Please contact us with any bug reports, feature requests, and rules issues that need addressed at our contact page.
         </Text>
-        <Image source={require('./../../assets/images/manIcon.png')}/>
+        <Image style={styles.icon} source={require('./../../assets/images/manIcon.png')}/>
       </View>
-      <View>
-        <Text>For new players:</Text>
-        <Text>Create your character</Text>
-        <Link href='./pages/generator/characterGenerator'>
-          <Pressable>
-            <Text>Character Creator</Text>
+      <View style={styles.characterCreationView}>
+        <Text style={styles.ccHeader}>For new players:</Text>
+        <Text style={styles.ccSubHeader}>Create your character</Text>
+        <Link style={styles.ccLink} href='./generator/characterGenerator' asChild>
+          <Pressable style={styles.ccButton}>
+            <Text style={styles.ccButtonText}>Character Creator</Text>
           </Pressable>
         </Link>
       </View>
     </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  descriptionView: {
+    flex: 2
+  }, 
+  header: {
+    fontSize: 30,
+    fontFamily: 'requiem',
+    textAlign: 'center'
+  },
+  subHeader: {
+    fontSize: 18,
+    fontFamily: 'requiem',
+    textAlign: 'center'
+  },
+  tertHeader: {
+    fontSize: 16,
+    fontFamily: 'requiem',
+    textAlign: 'center'
+  },
+  descText: {
+    fontSize: 14,
+    textAlign: 'center',
+    width: '95%',
+    alignSelf: 'center',
+    marginTop: 5,
+    marginBottom: 10
+  },
+  icon: {
+    width: 300,
+    height: 100,
+    alignSelf: 'center'
+  },
+  characterCreationView: {
+    flex: 1
+  },
+  ccHeader: {
+    fontSize: 20,
+    textAlign: 'center'
+  },
+  ccSubHeader: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 5
+  },
+  ccLink: {
+    alignSelf: 'center',
+    marginTop: 10
+  },
+  ccButton: {
+    backgroundColor: 'rgb(214, 176, 52)',
+    padding: 15,
+    marginTop: 20,
+    borderRadius: 5
+  },
+  ccButtonText: {
+    fontSize: 16,
+    fontFamily: 'requiem'
+  }
+})
--- cogitator/app/pages/generator/characterGenerator.tsx
+++ cogitator/app/pages/generator/characterGenerator.tsx
@@ -1,10 +1,27 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, Button } from 'react-native'
+import React, { useState } from 'react'
+
+
 
 export default function characterGenerator() {
+  let randomNumber = null;
+
+  // Generates a 'roll' of 2d10 + 20
+  function generateCharacteristic() {
+    const min = 1;
+    const max = 20;
+    return (Math.floor(Math.random() * max + min + 20));
+}
+
   return (
     <View>
-      <Text>characterGenerator</Text>
+      <Text>Who are You?</Text>
+      <Button title='Generate Characteristic Roll' onPress={function () {
+        randomNumber = generateCharacteristic();
+        console.log('running onpress: ' + randomNumber)
+      }}/>
+      {/* Console.log works but Text display does not. Because it is already rendered? */}
+      <Text>Result: {randomNumber}</Text>
     </View>
   )
 }
