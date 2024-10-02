a2f00e9077ff780cd885f9030456080dddcfddef
crosby7 <53411767+crosby7@users.noreply.github.com>
2024-10-01 23:05:49 -0400

    allow for characteristic rolls
    Currently, a button press 'rolls' each characteristic in order. Following this game mechanic, players will then be able to:
    - Swap stat assignments for reduced xp
    - Reroll and swap for no xp OR use point buy for no xp
    - Keep random rolls with random assignment for max xp

--- cogitator/app/pages/generator/characterGenerator.tsx
+++ cogitator/app/pages/generator/characterGenerator.tsx
@@ -4,24 +4,56 @@ import React, { useState } from 'react'
 
 
 export default function characterGenerator() {
-  let randomNumber = null;
+  let [strChar, setStrChar] = useState(0);
+  let [wsChar, setWsChar] = useState(0);
+  let [bsChar, setBsChar] = useState(0);
+  let [intChar, setIntChar] = useState(0);
+  let [felChar, setFelChar] = useState(0);
+  let [tghChar, setTghChar] = useState(0);
+  let [agChar, setAgChar] = useState(0);
+  let [perChar, setPerChar] = useState(0);
+  let [wilChar, setWilChar] = useState(0);
 
   // Generates a 'roll' of 2d10 + 20
   function generateCharacteristic() {
     const min = 1;
     const max = 20;
-    return (Math.floor(Math.random() * max + min + 20));
+    let randomNumber = Math.floor(Math.random() * max + min + 20);
+    setStrChar(randomNumber);
+    randomNumber = Math.floor(Math.random() * max + min + 20);
+    setWsChar(randomNumber);
+    randomNumber = Math.floor(Math.random() * max + min + 20);
+    setBsChar(randomNumber);
+    randomNumber = Math.floor(Math.random() * max + min + 20);
+    setIntChar(randomNumber);
+    randomNumber = Math.floor(Math.random() * max + min + 20);
+    setFelChar(randomNumber);
+    randomNumber = Math.floor(Math.random() * max + min + 20);
+    setTghChar(randomNumber);
+    randomNumber = Math.floor(Math.random() * max + min + 20);
+    setAgChar(randomNumber);
+    randomNumber = Math.floor(Math.random() * max + min + 20);
+    setPerChar(randomNumber);
+    randomNumber = Math.floor(Math.random() * max + min + 20);
+    setWilChar(randomNumber);
 }
 
   return (
     <View>
       <Text>Who are You?</Text>
       <Button title='Generate Characteristic Roll' onPress={function () {
-        randomNumber = generateCharacteristic();
-        console.log('running onpress: ' + randomNumber)
+        generateCharacteristic();
       }}/>
       {/* Console.log works but Text display does not. Because it is already rendered? */}
-      <Text>Result: {randomNumber}</Text>
+      <Text>Strength: {strChar}</Text>
+      <Text>Weapon Skill: {wsChar}</Text>
+      <Text>Ballistic Skill: {bsChar}</Text>
+      <Text>Intelligence: {intChar}</Text>
+      <Text>Fellowship: {felChar}</Text>
+      <Text>Agility: {agChar}</Text>
+      <Text>Willpower: {wilChar}</Text>
+      <Text>Perception: {perChar}</Text>
+      <Text>Toughness: {tghChar}</Text>
     </View>
   )
 }
